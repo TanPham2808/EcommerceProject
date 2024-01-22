ff44a14c705d04ab0ffcbd5774ee53c49cadca02
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-22 15:39:35 +0700

    Phân chia Area

--- Ecommerce.Web/Ecommerce.Web/Program.cs
+++ Ecommerce.Web/Ecommerce.Web/Program.cs
@@ -22,6 +22,8 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+//pattern: "{controller=Home}/{action=Index}/{id?}");
